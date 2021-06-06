dcd58ad886323a4cefda783b93000e9b3d37c12f
yibinna <48735656+yibinna@users.noreply.github.com>
2021-06-06 13:53:08 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -4,7 +4,7 @@ public class Card {
 private String suit;
 private String face;
 
-String MyfaceArray[] = {"1","2","3","4","5","6","7","8","9","10","Queen","King","Joker","Ace"};
+String MyfaceArray[] = {"1","2","3","4","5","6","7","8","9","10","Queen","King","Ace"};
 String MysuitArray[] = {"Hearts","Spades","Diamonds","Clubs"};
 
 
