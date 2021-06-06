a49d8f56d84990f0d568de2c3918217a05522656
yibinna <48735656+yibinna@users.noreply.github.com>
2021-06-06 13:47:38 -0400

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class Card {
+private String suit;
+private String face;
+
+String MyfaceArray[] = {"1","2","3","4","5","6","7","8","9","10","Jack","Queen","King","Joker","Ace"};
+String MysuitArray[] = {"Hearts","Spades","Diamonds","Clubs"};
+
+
+public Card (String suit, String face) {
+	
+	Random cardd = new Random();
+	int cardone = cardd.nextInt(MyfaceArray.length);
+	int cardtwo = cardd.nextInt(MysuitArray.length);	
+
+	this.face = MyfaceArray[cardone];
+	this.suit = MysuitArray[cardtwo];
+}
+public String toString() { 
+	String conjunction = " of ";
+	return face+ " " + conjunction + " " + suit; 
+}
+}
+
---
+++ HowManyCards.java
@@ -0,0 +1,11 @@
+
+public class HowManyCards {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=5; i++) {
+		Card card1 = new Card("","");
+		System.out.println(card1);
+
+	}
+	}
+}
